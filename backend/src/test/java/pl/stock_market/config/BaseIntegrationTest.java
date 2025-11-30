b6f1b1669cae729bc4d67aa33d2d17d62fcac68c
FuuKowatty <bartoszmech0@gmail.com>
2025-11-30 12:17:51 +0100

    refactor: fix integration tests and optimize docker build image

---
+++ backend/src/test/java/pl/stock_market/config/BaseIntegrationTest.java
@@ -0,0 +1,16 @@
+package pl.stock_market.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pl.stock_market.modules.holder.HolderFacade;
+import pl.stock_market.modules.wallet.WalletFacade;
+
+@SpringBootTest
+@Import(TestContainersConfiguration.class)
+public abstract class BaseIntegrationTest {
+    @MockitoBean
+    protected WalletFacade walletFacadeMock;
+    @MockitoBean
+    protected HolderFacade holderFacadeMock;
+}
